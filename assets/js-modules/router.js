133dab835a11addd051fe89e9cb19ba6dd70e56e
Gabriel-Santos01 <gabriel.adv45@outlook.com>
2023-04-17 19:39:21 -0300

    feat: add bg switch function

--- assets/js-modules/router.js
+++ assets/js-modules/router.js
@@ -1,3 +1,4 @@
+import bgChange from './bgChange.js'
 export class Router {
   routes = {}
 
@@ -11,6 +12,35 @@ export class Router {
 
     window.history.pushState({}, '', event.target.href)
     this.handle()
+
+    const getPathname = () => {
+      const path = window.location.pathname
+      return path.charAt(0) === '/' ? path.slice(1) : path
+    }
+
+    function setBodyBackground() {
+      const body = document.body
+      const path = getPathname()
+
+      // Verifica o pathname e define o background do body de acordo
+      switch (path) {
+        case 'universo':
+          body.style.backgroundImage =
+            'url(./assets/images/mountains-universe-2.png)'
+          break
+        case 'exploracao':
+          body.style.backgroundImage =
+            'url(./assets/images/mountains-universe-3.png)'
+          break
+        default:
+          // Define um background padrão para outras páginas
+          body.style.backgroundImage =
+            'url(./assets/images/mountains-universe-1.png)'
+          break
+      }
+    }
+
+    setBodyBackground()
   }
 
   handle() {
